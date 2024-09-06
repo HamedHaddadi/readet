14dae5f5b8ece586e818f20891300d5365707b74
HamedHaddadi <haddadi.h81@gmail.com>
2024-09-05 20:33:41 -0400

    knowledge graph and custom patent added

--- flm/documents/tools.py
+++ flm/documents/tools.py
@@ -2,7 +2,7 @@
 # custom tools for interacting with documents #
 # ########################################### #
 import os 
-from serpapi import GoogleScholarSearch 
+from serpapi import Client 
 from langchain.pydantic_v1 import BaseModel, Field, root_validator
 from langchain_core.tools import BaseTool  
 from typing import Optional, Any, Dict, Type   
@@ -13,7 +13,7 @@ class ScholarSearch(BaseModel):
 	"""
 	Wrapper for Serp API Google Scholar Search
 	Attributes:
-		top_k_results: number of results to return from goog-scholar query search
+		top_k_results: number of results to return from google-scholar query search
 			by defualt it returns 20 results.
 		serp_api_key: key for the serapi API call. It must be provided as a keyword argument or be available 
 			in the environment
@@ -28,22 +28,24 @@ class ScholarSearch(BaseModel):
 		serp_api_key = values.get('serp_api_key')
 		if serp_api_key is None:
 			serp_api_key = os.environ["SERP_API_KEY"]
-		GoogleScholarSearch.SERP_API_KEY = serp_api_key 
-		values["scholar_search_engine"] = GoogleScholarSearch 
+		client = Client(api_key = serp_api_key) 
+		values["scholar_search_engine"] = client 
 		return values 
 	
 	def run(self, query: str) -> str:
 		page = 0
 		all_results = []
 		while page < max((self.top_k_results - 20), 1):
-			results = (self.scholar_search_engine({"q": query, "start": page, "hl": "en",
+			results = (self.scholar_search_engine({"engine": "google_scholar"
+						,"q": query, "start": page, "hl": "en",
                         "num": min( self.top_k_results, 20), "lr": "lang_en"}).get_dict().get("organic_results", []))
 			all_results.extend(results)
 			if not results:  
 				break
 			page += 20
 		if (self.top_k_results % 20 != 0 and page > 20 and all_results):  # From the last page we would only need top_k_results%20 results
-			results = (self.search_scholar_engine({"q": query,"start": page,
+			results = (self.search_scholar_engine({"engine":"google_scholar",
+							 "q": query,"start": page,
 					"num": self.top_k_results % 20,
 					 "hl": "en", "lr": "lang_en"}).get_dict().get("organic_results", []))
 			all_results.extend(results)
@@ -75,6 +77,90 @@ class GoogleScholarTool(BaseTool):
 		Use the tool
 		"""
 		return self.api_wrapper.run(query)
+
+# ## Google Patent Search Tool ## #
+# API call					   ## #	 
+# ##						   ## # 
+class PatentSearch(BaseModel):
+	"""
+		Wrapper for Serp API Google Scholar Search
+	Attributes:
+		max_number_of_pages: maximum number of pages to peruse
+			by defualt it searches 10 pages.
+		serp_api_key: key for the serapi API call. It must be provided as a keyword argument or be available 
+			in the environment
+		patent_search_engine: serpapi Client object with engine defined as google_patents 
+	"""
+	max_number_of_pages: int = 10 
+	serp_api_key: Optional[str] = None 
+	patent_search_engine: Any 
+
+	@root_validator(pre = True)
+	def validate_environment_and_key(cls, values: Dict) -> Dict:
+		serp_api_key = values.get('serp_api_key')
+		if serp_api_key is None:
+			serp_api_key = os.environ["SERP_API_KEY"]
+		client = Client(api_key = serp_api_key)
+		values["patent_search_engine"] = client 
+		return values 
+	
+	def run(self, query:str) -> str:
+		patent_data = []
+		search_inputs = {"engine": "google_patents",
+                            "q": query,
+                             "page": 1, 
+                                "country": "US"}
+
+		for _ in range(self.max_number_of_pages):
+			organic_results = self.patent_search_engine.search(search_inputs)["organic_results"]
+			for result in organic_results:
+				data = {"title": result.get("title"), "patent_id": result.get("patent_id"), 
+                        "pdf": result.get("pdf"),
+                "priority_date": result.get("priority_date"), "filing_date": result.get("filing_date"), 
+                "grant_date": result.get("grant_date"), "publication_date": result.get("publication_date"), 
+                    "inventor": result.get("inventor"), "assignee": result.get("assignee")}
+            
+			search_inputs["page"] += 1
+			patent_data.append(data)
+		
+		patents = [f"""Title: {data.get("title")} \n
+                    PDF: {data.get("pdf")} \n
+                    Patent ID: {data.get("patent_id")} \n
+                        Priority Date: {data.get("priority_date")} \n 
+                            Filing Date: {data.get("filing date")} \n
+                            Grant Date: {data.get("grant_date")} \n
+                            Publication Date: {data.get("publication_date")} \n
+                            Inventor: {data.get("inventor")} \n
+                            Assignee: {data.get("assignee")} \n
+        """ for data in patent_data]
+		return "\n\n".join(patents)
+
+# Google Patent Tool 
+class GooglePatentTool(BaseTool):
+	"""
+	Tool that requires Google patent search API
+	"""
+	name: str = "google_patent_tool"
+	description: str = """A wrapper around Google Patent Search.
+        Useful for when you need to get information about
+        patents from Google Patents
+        Input should be a search query."""
+	api_wrapper: PatentSearch 
+
+	def _run(self, query:str) -> str:
+		"""
+		Use the tool
+		"""
+		return self.api_wrapper.run(query)
+
+
+
+
+
+
+
+
+
 
 
 
